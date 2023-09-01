0d3b9158027dd0ce76e5ddeb30263f58404ec1d1
Izuagbe Samuel Enakhe <izuagbesamuel73@gmail.com>
2023-09-01 18:34:55 +0100

    Add seperate classes for user information

---
+++ Migrations/20230901163818_UserFields.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdminPanel.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DOB",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HomeAddress",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAuthorized",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOnline",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLogin",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherAddress",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WorkAddress",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DOB",
+                schema: "Identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                schema: "Identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                schema: "Identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "HomeAddress",
+                schema: "Identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsAuthorized",
+                schema: "Identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsOnline",
+                schema: "Identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastLogin",
+                schema: "Identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "OtherAddress",
+                schema: "Identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WorkAddress",
+                schema: "Identity",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ Models/PersonalInfo.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+
+namespace AdminPanel.Models
+{
+    public class PersonalInfo
+    {
+        //Basic Infromation
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Gender { get; set; }
+        public byte[] ProfilePicture { get; set; }
+        public int UsernameChangeLimit { get; set; } = 10;
+    }
+}

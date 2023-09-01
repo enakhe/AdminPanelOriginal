4c5d4ddac7989326320f9158c54efa294f204ad2
Enakhe <izuagbesam@gmail.om>
2023-09-01 10:19:55 +0100

    BUGFIX: Successfully listed user profile picture on user data table

--- ViewModels/UserRolesViewModel.cs
+++ ViewModels/UserRolesViewModel.cs
@@ -12,6 +12,7 @@ namespace AdminPanel.ViewModels
         public string FullName { get; set; }
         public string Email { get; set; }
         public DateTime DateCreated { get; set; }
+        public byte[] ProfilePicture { get; set; }
         public IEnumerable<string> Roles { get; set; }
     }
 }

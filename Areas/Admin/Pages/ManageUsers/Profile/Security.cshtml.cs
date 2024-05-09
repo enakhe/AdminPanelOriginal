322ccdd0cc2234d86a56cb62d685576a950b3c49
Izuagbe Samuel Enakhe <izuagbesamuel73@gmail.com>
2024-05-09 13:58:00 +0100

    WIP: Work on the Security.cshtml.cs page to validat password

--- Areas/Admin/Pages/ManageUsers/Profile/Security.cshtml.cs
+++ Areas/Admin/Pages/ManageUsers/Profile/Security.cshtml.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Text;
 
 namespace AdminPanel.Areas.Admin.Pages.ManageUsers.Profile
@@ -21,14 +22,16 @@ namespace AdminPanel.Areas.Admin.Pages.ManageUsers.Profile
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ApplicationDbContext _db;
         private readonly IAuditLog _auditLog;
+        private readonly ILogger<SecurityModel> _logger;
 
 
-        public SecurityModel(UserManager<ApplicationUser> userManager, ApplicationDbContext db, SignInManager<ApplicationUser> signInManager, IAuditLog auditLog)
+        public SecurityModel(UserManager<ApplicationUser> userManager, ApplicationDbContext db, SignInManager<ApplicationUser> signInManager, IAuditLog auditLog, ILogger<SecurityModel> logger)
         {
             _userManager = userManager;
             _db = db;
             _signInManager = signInManager;
             _auditLog = auditLog;
+            _logger = logger;
         }
 
         [BindProperty]
@@ -63,14 +66,65 @@ namespace AdminPanel.Areas.Admin.Pages.ManageUsers.Profile
             }
         }
 
-        public async Task<IActionResult> OnPostAsync(ApplicationUser user, string newPassword)
+        public async Task<IActionResult> OnPostAsync(string Id, string newPassword)
         {
             _ = ReturnUrl;
-            ApplicationUser updatedUser = await _userManager.FindByIdAsync(user.Id);
+            ApplicationUser admin = await _userManager.GetUserAsync(User);
+            ApplicationUser user = await _userManager.FindByIdAsync(Id);
 
-            await _userManager.ChangePasswordAsync(updatedUser, "DummyUser$01", newPassword);
-            await _userManager.UpdateAsync(updatedUser);
-            StatusMessage = "Successfully created user profile";
+            var result = await _userManager.ChangePasswordAsync(user, "DummyUser$01", newPassword);
+
+            if(result.Succeeded)
+            {
+                await _userManager.UpdateAsync(user);
+                StatusMessage = "Successfully updated user password";
+
+                // Add Audit Device Information
+                var continent = await _auditLog.GetContinent();
+                var countryName = await _auditLog.GetCountryName();
+                var country = await _auditLog.GetCountry();
+                var city = await _auditLog.GetCity();
+                var state = await _auditLog.GetState();
+                AuditDeviceInfo auditDeviceInfo = new()
+                {
+                    DeviceType = _auditLog.GetDeviceType(HttpContext),
+                    OperatingSystem = _auditLog.GetOperatingSystem(HttpContext),
+                    BrowserName = _auditLog.GetBrowserName(HttpContext),
+                    BrowserVersion = _auditLog.GetBrowserVersion(HttpContext),
+                    IPAddress = _auditLog.GetIpAddress(HttpContext),
+                    DeviceContinent = continent,
+                    DeviceCountryName = countryName,
+                    DeviceCountry = country,
+                    DeviceCity = city,
+                    DeviceState = state,
+                };
+                await _db.AuditDeviceInfo.AddAsync(auditDeviceInfo);
+
+                // Add Audit Loggin Information
+                AuditLogging auditLogging = new()
+                {
+                    AdminId = admin.Id,
+                    User = user,
+                    UserId = user.Id,
+                    DeviceInfoId = auditDeviceInfo.Id,
+                    AuditDeviceInfo = auditDeviceInfo,
+                    AuditActionType = "Post",
+                    StatusMessage = StatusMessage
+                };
+                await _db.AuditLoggings.AddAsync(auditLogging);
+
+                await _db.SaveChangesAsync();
+
+                LoadAsync(user);
+            } 
+            else
+            {
+                var error = result.Errors.ToList()[0].Description;
+                _logger.LogError(error);
+                StatusMessage = $"Error, {error}";
+                LoadAsync(user);
+                return Page();
+            }
 
             LoadAsync(user);
             return Page();
---
+++ Migrations/20240509113903_AuditLogging Time.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdminPanel.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditLoggingTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
